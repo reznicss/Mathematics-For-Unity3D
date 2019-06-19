440ec76a6a540cd67262ba78347ba13131fbbae3
reznicss <reznicss@163.com>
2019-06-20 05:07:05 +0800

    Vector3

---
+++ Assets/Tools/Math3D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Math3D  {
+
+    public static float V3Magnitude(Vector3 a)
+    {
+        return Mathf.Sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    }
+
+    public static float V3SqrMagnitude(Vector3 a)
+    {
+        return a.x * a.x + a.y * a.y + a.z * a.z;
+    }
+
+    public static float V3Dot(Vector3 a,Vector3 b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    public static float V3Angle(Vector3 a, Vector3 b)
+    {
+        var f1 = (a.x * b.x + a.y * b.y + a.z * b.z);
+        var f2 = Mathf.Sqrt(a.x * a.x + a.y * a.y + a.z * a.z) * Mathf.Sqrt(b.x * b.x + b.y * b.y + b.z * b.z);
+        //Mathf.Deg2Rad 表示从角度到弧度转变的常量值, 其值为(2 * Mathf.PI)/ 360
+        //Mathf.Rad2Deg 表示从弧度到角度转变的常量值, 其值为360/ (2 * Mathf.PI)
+        return Mathf.Rad2Deg * Mathf.Acos(f1 / f2) ;
+    }
+}
---
+++ Assets/Vector/VectorTest.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorTest : MonoBehaviour {
+
+	void Start () {
+        var v1 = new Vector3(1, 1, 0);
+        var v2 = new Vector3(1, 0, 0);
+
+        print("*******向量长度*******");
+        print(v1.magnitude + "_" + Math3D.V3Magnitude(v1));
+
+        print("*******向量长度平方*******");
+        print(v1.sqrMagnitude + "_" + Math3D.V3SqrMagnitude(v1));
+
+        print("*******向量点乘(内积)*******");
+        print(Vector3.Dot(v1,v2) + "_" + Math3D.V3Dot(v1,v2));
+
+        print("*******向量夹角()*******");
+        print(Vector3.Angle(v1, v2) + "_" + Math3D.V3Angle(v1, v2));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
