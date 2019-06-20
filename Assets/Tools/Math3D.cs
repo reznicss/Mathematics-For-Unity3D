81d0398828ee023e184f601b2ef54b055feb241a
reznicss <reznicss@163.com>
2019-06-20 22:35:49 +0800

    向量更新角度算法及公式

--- Assets/Tools/Math3D.cs
+++ Assets/Tools/Math3D.cs
@@ -2,29 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Math3D  {
+/**
+ * 1.‖P + Q‖ ≤ ‖P‖+ ‖Q‖     几何意义，三角形的两边之和大于第三边
+ * 2.‖P· Q‖ ≤ ‖P‖‖Q‖       Cauchy-Schwarz不等式
+ * 3. P·Q = ‖P‖‖Q‖cosα     根据余弦定理推得(‖P - Q‖² = ‖P‖²+ ‖Q‖² - 2||P|| ||Q|| cosα)
+ * */
 
+public class Math3D
+{
+
+    /// <summary>
+    /// 模
+    /// </summary>
     public static float V3Magnitude(Vector3 a)
     {
         return Mathf.Sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
     }
 
+    /// <summary>
+    /// 模平方
+    /// </summary>
     public static float V3SqrMagnitude(Vector3 a)
     {
         return a.x * a.x + a.y * a.y + a.z * a.z;
     }
 
-    public static float V3Dot(Vector3 a,Vector3 b)
+    /// <summary>
+    /// 点乘
+    /// </summary>
+    public static float V3Dot(Vector3 a, Vector3 b)
     {
+        //大于0同向，小鱼0反向，等于0垂直
         return a.x * b.x + a.y * b.y + a.z * b.z;
     }
 
+    /// <summary>
+    /// 夹角(0到180)
+    /// </summary>
     public static float V3Angle(Vector3 a, Vector3 b)
     {
+
+        //Mathf.Deg2Rad 表示从角度到弧度转变的常量值, 其值为(2 * Mathf.PI) / 360
+        //Mathf.Rad2Deg 表示从弧度到角度转变的常量值, 其值为360 / (2 * Mathf.PI)
         var f1 = (a.x * b.x + a.y * b.y + a.z * b.z);
         var f2 = Mathf.Sqrt(a.x * a.x + a.y * a.y + a.z * a.z) * Mathf.Sqrt(b.x * b.x + b.y * b.y + b.z * b.z);
-        //Mathf.Deg2Rad 表示从角度到弧度转变的常量值, 其值为(2 * Mathf.PI)/ 360
-        //Mathf.Rad2Deg 表示从弧度到角度转变的常量值, 其值为360/ (2 * Mathf.PI)
-        return Mathf.Rad2Deg * Mathf.Acos(f1 / f2) ;
+        return Mathf.Rad2Deg * Mathf.Acos(f1 / f2);
     }
+
+    /// <summary>
+    /// 夹角(0到180 带正负)
+    /// </summary>
+    public static float V3SignedAngle(Vector3 a, Vector3 b, Vector3 axis)
+    {
+        float angle = Vector3.Angle(a, b);
+        angle *= Mathf.Sign(Vector3.Cross(a, b).y);
+        return angle;
+    }
+
+    public static float V3SignedAngle(Vector3 a, Vector3 b)
+    {
+        float angle = Vector3.Angle(a, b);
+        angle *= Mathf.Sign(Vector3.Cross(a, b).y);
+        return angle;
+    }
+
+
+    /// <summary>
+    /// 夹角(0到360,axis为(0, 1, 0)时顺时针,(0,-1,0)逆时针)
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="axis"></param>
+    /// <returns></returns>
+    public static float V3SignedAngleBetween(Vector3 a, Vector3 b, Vector3 axis)
+    {
+        float angle = Vector3.Angle(a, b);
+        float sign = Mathf.Sign(Vector3.Dot(axis, Vector3.Cross(a, b)));
+        float signed_angle = angle * sign;
+        return (signed_angle <= 0) ? 360 + signed_angle : signed_angle;
+    }
+
+
 }
--- Assets/Vector/VectorTest.cs
+++ Assets/Vector/VectorTest.cs
@@ -3,28 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VectorTest : MonoBehaviour {
+public class VectorTest : MonoBehaviour
+{
+    public Transform t1;
+    public Transform t2;
+    public Vector3 v1;
+    public Vector3 v2;
 
-	void Start () {
-        var v1 = new Vector3(1, 1, 0);
-        var v2 = new Vector3(1, 0, 0);
 
-        print("*******向量长度*******");
-        print(v1.magnitude + "_" + Math3D.V3Magnitude(v1));
+    private void Start()
+    {
+        print("向量长度:" + v1.magnitude + "_" + Math3D.V3Magnitude(v1));
+        print("向量长度平方:" + v1.sqrMagnitude + "_" + Math3D.V3SqrMagnitude(v1));
 
-        print("*******向量长度平方*******");
-        print(v1.sqrMagnitude + "_" + Math3D.V3SqrMagnitude(v1));
+        print("向量点乘(内积):" + Vector3.Dot(v1, v2) + "_" + Math3D.V3Dot(v1, v2));
+        print("向量夹角(0到180):" + Vector3.Angle(v1, v2) + "_" + Math3D.V3Angle(v1, v2));
+        print("向量夹角(0到180 带正负):" + Vector3.SignedAngle(v2, v1, Vector3.up) + "_" + Math3D.V3SignedAngle(v2, v1, Vector3.up));
+        print("向量夹角(0到360):" + Math3D.V3SignedAngleBetween(v2, v1, Vector3.up));
 
-        print("*******向量点乘(内积)*******");
-        print(Vector3.Dot(v1,v2) + "_" + Math3D.V3Dot(v1,v2));
-
-        print("*******向量夹角()*******");
-        print(Vector3.Angle(v1, v2) + "_" + Math3D.V3Angle(v1, v2));
 
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void OnDrawGizmos()
+    {
+        v1 = t1.position;
+        v2 = t2.position;
+        Gizmos.DrawSphere(v1, 0.05f);
+        Gizmos.DrawSphere(v2, 0.05f);
+        Gizmos.DrawLine(Vector3.zero, v1);
+        Gizmos.DrawLine(Vector3.zero, v2);
+        //Gizmos.DrawLine(v1, v2);
+    }
 }
