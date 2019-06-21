0abd5a8bc51b3b7bef2524e65d5fc227c6ecc67e
reznicss <reznicss@163.com>
2019-06-21 17:29:25 +0800

    向量投影,叉乘

--- Assets/Tools/Math3D.cs
+++ Assets/Tools/Math3D.cs
@@ -6,11 +6,19 @@ using UnityEngine;
  * 1.‖P + Q‖ ≤ ‖P‖+ ‖Q‖     几何意义，三角形的两边之和大于第三边
  * 2.‖P· Q‖ ≤ ‖P‖‖Q‖       Cauchy-Schwarz不等式
  * 3. P·Q = ‖P‖‖Q‖cosα     根据余弦定理推得(‖P - Q‖² = ‖P‖²+ ‖Q‖² - 2||P|| ||Q|| cosα)
+ * 4. P·P = ‖P‖²
+ * 5. P×Q·P =  P×Q·Q = 0
+ * 6.‖P×Q‖=‖P‖‖Q‖sinα (平行四边形)
+ * 7. Area = 1/2 *‖(V2 - V1)×(V3 - V1)‖ (由V1,V2,V3三个顶点组成的任意三角形面积
+ * 8. P×Q = -(Q×P) 
+ * 9. P×P = (0,0,0)
+ * 10.(P×Q)·R = (R×P)·Q =(Q×R)·P
+ * 11.P×(Q+R) = P×Q + P×R
+ * 12.P×(Q×P) = P×Q×P = P²Q -(P·Q)P
  * */
 
 public class Math3D
 {
-
     /// <summary>
     /// 模
     /// </summary>
@@ -41,7 +49,7 @@ public class Math3D
     /// </summary>
     public static float V3Angle(Vector3 a, Vector3 b)
     {
-
+        //P·Q = ‖P‖‖Q‖cosα
         //Mathf.Deg2Rad 表示从角度到弧度转变的常量值, 其值为(2 * Mathf.PI) / 360
         //Mathf.Rad2Deg 表示从弧度到角度转变的常量值, 其值为360 / (2 * Mathf.PI)
         var f1 = (a.x * b.x + a.y * b.y + a.z * b.z);
@@ -58,7 +66,6 @@ public class Math3D
         angle *= Mathf.Sign(Vector3.Cross(a, b).y);
         return angle;
     }
-
     public static float V3SignedAngle(Vector3 a, Vector3 b)
     {
         float angle = Vector3.Angle(a, b);
@@ -66,14 +73,9 @@ public class Math3D
         return angle;
     }
 
-
     /// <summary>
     /// 夹角(0到360,axis为(0, 1, 0)时顺时针,(0,-1,0)逆时针)
     /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <param name="axis"></param>
-    /// <returns></returns>
     public static float V3SignedAngleBetween(Vector3 a, Vector3 b, Vector3 axis)
     {
         float angle = Vector3.Angle(a, b);
@@ -82,5 +84,62 @@ public class Math3D
         return (signed_angle <= 0) ? 360 + signed_angle : signed_angle;
     }
 
+    /// <summary>
+    /// 向量投影(向量a在向量b上投影)
+    /// </summary>
+    public static Vector3 V3Project(Vector3 a, Vector3 b)
+    {
+        // cosα  = a · b / (‖a‖‖b‖) = ‖projB‖/‖a‖
+        // => ‖projB‖ =  a · b / (‖a‖‖b‖) * ‖a‖
+        // => projB = a · b / (‖a‖‖b‖) * ‖a‖ * ( b /‖b‖) 
+        return b * Vector3.Dot(a,b) / Vector3.Dot(b,b);
+    }
+
+    /// <summary>
+    /// 向量投影(a在法线为b的平面上的投影向量,即向量a相对于向量b的垂直向量)
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static Vector3 V3ProjectOnPlane(Vector3 a, Vector3 b)
+    {
+        return a - b * Vector3.Dot(a, b) / Vector3.Dot(b, b);
+    }
+
+    /// <summary>
+    /// 按照轨道朝目标的运动方向
+    /// </summary>
+    /// <param name="selfPos">当前位置</param>
+    /// <param name="tarPos">目标对象位置</param>
+    /// <param name="railDirection">轨道位置</param>
+    /// <returns>运动方向</returns>
+    public static Vector3 V3MoveDirectionByProject(Vector3 selfPos, Vector3 tarPos, Vector3 railDirection)
+    {
+        var heading = tarPos - selfPos;
+        var force = Vector3.Project(heading, railDirection);
+        return force;
+    }
+
+    /// <summary>
+    /// 叉乘
+    /// </summary>
+    public static Vector3 V3Cross(Vector3 a, Vector3 b)
+    {
+        var c = new Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+        //遵守左手定律(Unity为左手坐标系)
+        if (c.y > 0)
+        {
+            // b在a的顺时针
+        }
+        else if (c.y == 0)
+        {
+            // b和a平行
+        }
+        else
+        {
+            // b在a的逆时针
+        }
+        return c;
+    }
 
 }
--- Assets/Vector/VectorTest.cs
+++ Assets/Vector/VectorTest.cs
@@ -7,31 +7,33 @@ public class VectorTest : MonoBehaviour
 {
     public Transform t1;
     public Transform t2;
-    public Vector3 v1;
-    public Vector3 v2;
-
 
     private void Start()
     {
-        print("向量长度:" + v1.magnitude + "_" + Math3D.V3Magnitude(v1));
-        print("向量长度平方:" + v1.sqrMagnitude + "_" + Math3D.V3SqrMagnitude(v1));
+        print("向量长度:" + t1.position.magnitude + "_" + Math3D.V3Magnitude(t1.position));
+        print("向量长度平方:" + t1.position.sqrMagnitude + "_" + Math3D.V3SqrMagnitude(t1.position));
 
-        print("向量点乘(内积):" + Vector3.Dot(v1, v2) + "_" + Math3D.V3Dot(v1, v2));
-        print("向量夹角(0到180):" + Vector3.Angle(v1, v2) + "_" + Math3D.V3Angle(v1, v2));
-        print("向量夹角(0到180 带正负):" + Vector3.SignedAngle(v2, v1, Vector3.up) + "_" + Math3D.V3SignedAngle(v2, v1, Vector3.up));
-        print("向量夹角(0到360):" + Math3D.V3SignedAngleBetween(v2, v1, Vector3.up));
+        print("向量点乘(内积):" + Vector3.Dot(t1.position, t2.position) + "_" + Math3D.V3Dot(t1.position, t2.position));
+        print("向量夹角(0到180):" + Vector3.Angle(t1.position, t2.position) + "_" + Math3D.V3Angle(t1.position, t2.position));
+        print("向量夹角(0到180 带正负):" + Vector3.SignedAngle(t2.position, t1.position, Vector3.up) + "_" + Math3D.V3SignedAngle(t2.position, t1.position, Vector3.up));
+        print("向量夹角(0到360):" + Math3D.V3SignedAngleBetween(t2.position, t1.position, Vector3.up));
 
+        print("向量投影(a在b上投影):" + Vector3.Project(t1.position, t2.position) + "_" + Math3D.V3Project(t1.position, t2.position));
+        print("向量投影(a在法线为n的平面上的投影向量):" + Vector3.ProjectOnPlane(t1.position,Vector3.forward) + "_" + Math3D.V3ProjectOnPlane(t1.position,Vector3.forward));
+
+        print("向量叉乘(外积):" + Vector3.Cross(t1.position,t2.position) + "_" + Math3D.V3Cross(t1.position, t2.position));
+        Debug.DrawLine(Vector3.zero, Math3D.V3Cross(t1.position, t2.position), Color.black, 1000f);
 
     }
 
     void OnDrawGizmos()
     {
-        v1 = t1.position;
-        v2 = t2.position;
-        Gizmos.DrawSphere(v1, 0.05f);
-        Gizmos.DrawSphere(v2, 0.05f);
-        Gizmos.DrawLine(Vector3.zero, v1);
-        Gizmos.DrawLine(Vector3.zero, v2);
-        //Gizmos.DrawLine(v1, v2);
+        t1.position = t1.position;
+        t2.position = t2.position;
+        Gizmos.DrawSphere(t1.position, 0.05f);
+        Gizmos.DrawSphere(t2.position, 0.05f);
+        Gizmos.DrawLine(Vector3.zero, t1.position);
+        Gizmos.DrawLine(Vector3.zero, t2.position);
+        //Gizmos.DrawLine(t1.position, t2.position);
     }
 }
